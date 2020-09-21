22a6e351323d9f4114751350a2cc94163672a881
Jayant2907 <gangwanijayant737@gmail.com>
2020-09-21 13:35:40 +0530

    Updates

---
+++ precache-manifest.a578ffdb56457638d441b30848b1fbc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4d5ed3be4c1fd8c9c300ad5d02461fe",
+    "url": "/Covid19-tracker/index.html"
+  },
+  {
+    "revision": "3faf42179ff2b6b9319f",
+    "url": "/Covid19-tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "482058efd72f9bd01a27",
+    "url": "/Covid19-tracker/static/css/main.d5142844.chunk.css"
+  },
+  {
+    "revision": "3faf42179ff2b6b9319f",
+    "url": "/Covid19-tracker/static/js/2.239ea4ca.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/Covid19-tracker/static/js/2.239ea4ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "482058efd72f9bd01a27",
+    "url": "/Covid19-tracker/static/js/main.0ae14303.chunk.js"
+  },
+  {
+    "revision": "29a935a113a42cf373bc",
+    "url": "/Covid19-tracker/static/js/runtime-main.14178214.js"
+  }
+]);
